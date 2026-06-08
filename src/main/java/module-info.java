e00fae3643fd2515773e70b125dab4323a6ecc18
francograziuso <f.graziuso2@studenti.unisa.it>
2026-06-08 12:08:57 +0200

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module com.mycompany.provaf {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.provaf to javafx.fxml;
-    exports com.mycompany.provaf;
-}
-*/
